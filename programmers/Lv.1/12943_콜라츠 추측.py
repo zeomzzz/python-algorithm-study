ed0baa91b45c57cf2040eedc55c5b1e311fb4c06
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-11-25 19:23:50 +0900

    Create 12943_콜라츠 추측.py

---
+++ programmers/Lv.1/12943_콜라츠 추측.py
@@ -0,0 +1,17 @@
+def solution(num):
+    answer = 0
+    
+    if num == 1 :
+        answer = 0
+    else :
+        while num != 1 and answer < 500 :
+            if num % 2 == 0 :
+                num = num // 2
+            else :
+                num = num * 3 + 1
+            answer += 1
+    
+    if answer == 500 :
+        answer = -1
+    
+    return answer
