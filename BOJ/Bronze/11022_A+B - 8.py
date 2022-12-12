c2d1474ca9ec21196e5150e27c2add4a130f3cce
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-13 00:25:47 +0900

    Update 11022_A+B - 8.py

--- BOJ/Bronze/11022_A+B - 8.py
+++ BOJ/Bronze/11022_A+B - 8.py
@@ -1,3 +1,5 @@
+# 풀이 1
+
 n = int(input())
 
 i = 0
@@ -5,4 +7,16 @@ i = 0
 while i < n :
     x, y = map(int, input().split())
     print("Case #" + str(i + 1) + ": " + str(x) + " + " + str(y) + " = " + str(x + y))
+    i += 1
+
+
+# 풀이 2
+
+n = int(input())
+
+i = 0
+
+while i < n :
+    x, y = map(int, input().split())
+    print(f'Case #{i + 1}: {x} + {y} = {x+y}')
     i += 1
