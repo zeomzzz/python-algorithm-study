283a30857affdf143bfd23d84b94e816678bdd4c
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-20 23:17:44 +0900

    Update 10870_피보나치 수 5.py

--- BOJ/Bronze/10870_피보나치 수 5.py
+++ BOJ/Bronze/10870_피보나치 수 5.py
@@ -1,14 +1,34 @@
+# 풀이 1 : 재귀함수
+
 def f(n) :
-    if n == 0 :
-        result = 0
-    elif n == 1 :
-        result = 1
+    if n <= 1 :
+        result = n
     elif n > 0 :
         result = f(n-1) + f(n-2)
-        
     return result
 
-  
-x = int(input())
 
+x = int(input())
 print(f(x))
+
+
+# 풀이 2 : for 문 01
+
+n = int(input())
+
+f = [0, 1]
+
+for i in range(2, n+1) :
+    f.append(f[i-1] + f[i-2])
+
+print(f[n])
+
+
+# 풀이 3 : for 문 02
+
+a, b = 0, 1
+
+for i in range(int(input())) :
+    a, b = b, a+b
+
+print(a)
