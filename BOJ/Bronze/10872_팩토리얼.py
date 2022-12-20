158671c4d7c7159a5c8a715d8551e2914e756c19
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-20 19:42:04 +0900

    Update 10872_팩토리얼.py

--- BOJ/Bronze/10872_팩토리얼.py
+++ BOJ/Bronze/10872_팩토리얼.py
@@ -1,3 +1,5 @@
+# 풀이 1 : for 문
+
 n = int(input())
 x = 1
 
@@ -5,3 +7,17 @@ for i in range(n) :
     x *= n - i
 
 print(x)
+
+
+# 풀이 2 : 재귀함수
+
+def factorial(n) :
+    result = 1
+
+    if n > 0 :
+        result = n * factorial(n-1)
+    
+    return result
+
+n = int(input())
+print(factorial(n))
