8875ba816aa7d32d3fdc9ab98fd974211e2e0aaf
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-21 18:10:37 +0900

    Update 2798 블랙잭.py

--- BOJ/Bronze/2798 블랙잭.py
+++ BOJ/Bronze/2798 블랙잭.py
@@ -1,3 +1,5 @@
+# 풀이 1 : for문 이용
+
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 lst = []
@@ -11,5 +13,22 @@ for i in range(n-2) :
                 continue
             else :
                 lst.append(sum)
+
+print(max(lst))
+
+
+# 풀이 2 : itertools 이용
+
+import itertools
+
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+lst = []
+
+for i in itertools.combinations(cards, 3) :
+    if sum(i) > m :
+        continue
+    else :
+        lst.append(sum(i))
 
 print(max(lst))
