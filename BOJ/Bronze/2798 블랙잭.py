f9e23877762c7f568d8ee1053cc349bd9985d44e
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-21 18:17:02 +0900

    Update 2798 블랙잭.py

--- BOJ/Bronze/2798 블랙잭.py
+++ BOJ/Bronze/2798 블랙잭.py
@@ -1,4 +1,4 @@
-# 풀이 1 : for문 이용
+# 풀이 1 : for문
 
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
@@ -17,7 +17,7 @@ for i in range(n-2) :
 print(max(lst))
 
 
-# 풀이 2 : itertools 이용
+# 풀이 2 : itertools
 
 import itertools
 
@@ -32,3 +32,20 @@ for i in itertools.combinations(cards, 3) :
         lst.append(sum(i))
 
 print(max(lst))
+
+
+# 풀이 3 : 브루트 포스 알고리즘
+
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+x = 0
+
+for i in range(n-2) :
+    for j in range(i+1, n-1) :
+        for k in range(j+1, n) :
+            sum = cards[i] + cards[j] + cards[k]
+
+            if sum <= m :
+                x = max(x, sum)
+
+print(x)
