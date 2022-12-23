182e289eb3f7586b56d9bb2890a366febc014b07
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-23 19:49:59 +0900

    Update 1288 새로운 불면증 치료법.py

--- SWEA/D2/1288 새로운 불면증 치료법.py
+++ SWEA/D2/1288 새로운 불면증 치료법.py
@@ -3,14 +3,15 @@ T = int(input())
 for i in range(T) :
     N = int(input())
     nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    j = 1
+    j = 0
 
     while True :
+        j += 1
+        
         for k in str(j * N) :
             if k in nums :
                 nums.remove(k)
-        j += 1
 
         if nums == [] :
-            print(f'#{i + 1} {(j - 1 ) * N}')
+            print(f'#{i + 1} {j * N}')
             break
