301946a8456670bd1dc7bc5a8ba643d5966674c7
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2022-12-26 20:26:51 +0900

    Update 1037 약수.py

--- BOJ/Bronze/1037 약수.py
+++ BOJ/Bronze/1037 약수.py
@@ -1,6 +1,17 @@
+# 풀이 1 : 정렬
+
 N = int(input())
 
 nums = list(map(int, input().split()))
 nums.sort()
 
 print(nums[0] * nums[N-1])
+
+
+# 풀이 2 : min, max
+
+N = int(input())
+
+nums = list(map(int, input().split()))
+
+print(min(nums) * max(nums))
