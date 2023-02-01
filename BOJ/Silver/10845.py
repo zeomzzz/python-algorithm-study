424adc4d7068063c0c7839644fcf58b120ac7489
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-02-02 01:28:35 +0900

    Create 10845.py

---
+++ BOJ/Silver/10845.py
@@ -0,0 +1,33 @@
+import sys
+
+N = int(sys.stdin.readline())
+queue = []
+
+for _ in range(N) :
+    cmd = sys.stdin.readline().rstrip().split()
+
+    if cmd[0] == "push" :
+        queue.append(cmd[1])
+    elif cmd[0] == "pop" :
+        if len(queue) > 0 :
+            print(queue.pop(0))
+        else :
+            print(-1)
+    elif cmd[0] == "size" :
+        print(len(queue))
+    elif cmd[0] == "empty" :
+        if len(queue) == 0 :
+            print(1)
+        else :
+            print(0)
+    elif cmd[0] == "front" :
+        if len(queue) > 0 :
+            print(queue[0])
+        else :
+            print(-1)
+    elif cmd[0] == "back" :
+        if len(queue) > 0 :
+            print(queue[-1])
+        else :
+            print(-1)
+
