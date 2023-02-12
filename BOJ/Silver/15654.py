781430813c75f0def03b0f777a508c938a069646
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-02-12 20:42:00 +0900

    Create 15654.py

---
+++ BOJ/Silver/15654.py
@@ -0,0 +1,32 @@
+'''
+solution(N, M) 이라고 할 때
+1) M == 1 : [1], [2], ... [N]
+2) else : solution(N, M-1) 각 요소에 [1, 2, ..., N] 중 없는 정수를 추가
+'''
+
+import sys
+
+def solution(N, M) :
+    ans = []
+
+    if M == 1 :
+        for i in arr :
+            ans.append([i])
+    else :
+        for i in solution(N, M-1) :
+            for j in arr :
+                if j not in i :
+                    tmp_arr = i[:]
+                    tmp_arr.append(j)
+                    ans.append(tmp_arr)
+
+    return ans
+
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+arr = list(map(int, sys.stdin.readline().rstrip().split()))
+arr = sorted(arr)
+
+for i in solution(n, m) :
+    i_tmp = list(map(str, i))
+    print(' '.join(i_tmp))
