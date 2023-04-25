48d333d4f5932d590d1f50c1c91b7fdd24664fa1
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-04-25 23:08:22 +0900

    Update 1406.py

--- BOJ/Silver/1406.py
+++ BOJ/Silver/1406.py
@@ -1,26 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-stack = list(input().rstrip()) # 처음에 위치가 맨 뒤, 즉 문자열이 다 왼쪽에
-lst = []
+left = list(input().rstrip()) # 처음에 위치가 맨 뒤, 즉 문자열이 다 왼쪽에
+right = []
 M = int(input().rstrip())  # 명령문의 개수 M
-# stack이 커서의 왼쪽, lst가 커서의 오른쪽 (lst는 append, pop 사용하기 위해 역순!!)
+# right는 append, pop 사용하기 위해 역순!!
 
 for _ in range(M) :
     cmd = input().rstrip().split()
 
     if cmd[0] == 'L' :
-        if len(stack) : # 맨 앞 아니면
-            lst.append(stack.pop())
+        if left : # 맨 앞 아니면
+            right.append(left.pop())
     elif cmd[0] == 'D' :
-        if len(lst) : # 맨 뒤 아니면
-            stack.append(lst.pop())
+        if right : # 맨 뒤 아니면
+            left.append(right.pop())
     elif cmd[0] == 'B' :
-        if len(stack) : # 맨 앞 아니면
-            stack.pop()
+        if left : # 맨 앞 아니면
+            left.pop()
     elif cmd[0] == 'P' :
-        stack.append(cmd[1])
+        left.append(cmd[1])
 
-lst = lst[::-1]
-for s in stack : print(s, end="")
-for l in lst : print(l, end="")
+print("".join(left), end="")
+print("".join(reversed(right)))
