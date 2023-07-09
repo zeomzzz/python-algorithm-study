276ae83c7107ac79e40f2f3c7a54b754220d9669
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-07-09 16:06:48 +0900

    Update 14719.py

--- BOJ/Gold/14719.py
+++ BOJ/Gold/14719.py
@@ -1,3 +1,4 @@
+# 풀이 1
 import sys
 input = sys.stdin.readline
 
@@ -22,3 +23,27 @@ for i in range(1, W - 1) :
         else : flag = False
 
 print(water)
+
+
+# 풀이 2
+
+import sys
+input = sys.stdin.readline
+
+H, W = map(int, input().rstrip().split()) # 세로 H, 가로 W
+blocks = list(map(int, input().rstrip().split()))
+
+# i번째 블럭에 빗물 고이는 양
+# i 이전 블럭 최대 높이, 이후 블럭 최대 높이 중 낮은 블럭과 현재 블럭의 차이 만큼
+
+rain = 0
+
+for i in range(1, W - 1) :
+    left = max(blocks[:i])
+    right = max(blocks[i:])
+
+    tmp = min(left, right)
+
+    if tmp > blocks[i] : rain += tmp - blocks[i]
+
+print(rain)
