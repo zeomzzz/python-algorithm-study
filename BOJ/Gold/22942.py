c51c62c681c1e59594b71abe498d663b624d30ed
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-07-12 21:07:30 +0900

    Update 22942.py

--- BOJ/Gold/22942.py
+++ BOJ/Gold/22942.py
@@ -1,3 +1,4 @@
+# 우큐 정렬하고 스택 짝 맞추기
 import sys
 from queue import PriorityQueue
 input = sys.stdin.readline
@@ -29,3 +30,34 @@ while pq.qsize() != 0 :
 
 if len(stack) : print("NO")
 else : print("YES")
+
+
+# 왼쪽 x좌표로 정렬 후 원 두 개 씩 비교
+import sys
+from queue import PriorityQueue
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+
+# o1left o2left o2right o1right
+
+circles = []
+
+for _ in range(N) :
+    x, r = map(int, input().rstrip().split())
+    circles.append([x - r, x + r])
+
+circles.sort()
+
+ans = True
+
+for i in range(N - 1) :
+    o1_left, o1_right = circles[i][0], circles[i][1]
+    o2_left, o2_right = circles[i + 1][0], circles[i + 1][1]
+
+    if o1_left == o2_left or o1_right == o2_right or o2_left < o1_right < o2_right :
+        ans = False
+        break
+
+if ans : print("YES")
+else : print("NO")
