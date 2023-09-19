ec2a0278caa090d668560b565b89243b92ea6c9a
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-09-19 22:15:51 +0900

    Update 12941.py

--- programmers/Lv.2/12941.py
+++ programmers/Lv.2/12941.py
@@ -9,3 +9,23 @@ def solution(A, B):
         answer += A[i] * B[i]
 
     return answer
+
+# 처음에 썼던 backtracking 코드 (시간초과로 런타임에러)
+# def backtracking(idx, depth, result, A, B):
+#     global visited, answer
+#
+#     if result >= answer: return
+#
+#     if idx == depth:
+#         if result < answer: answer = result
+#         return
+#
+#     for i in range(idx):
+#         if visited[i] == 0:  # 아직 idx 번째는 방문 안했으면
+#             result += A[depth] * B[i]
+#             visited[i] = 1
+#
+#             backtracking(idx, depth + 1, result, A, B)
+#
+#             result -= A[depth] * B[i]
+#             visited[i] = 0
