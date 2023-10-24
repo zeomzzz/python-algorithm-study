fd981c916f31c8d4ff1af5431cdb08ec49b03b25
Jeongmin <107301060+zeomzzz@users.noreply.github.com>
2023-10-24 18:57:52 +0900

    Update 43163.py

--- programmers/Lv.3/43163.py
+++ programmers/Lv.3/43163.py
@@ -44,4 +44,43 @@ def backtracking(cur, depth, target, words) :
             visited[idx] = 1
             backtracking(words[idx], depth + 1, target, words)
             visited[idx] = 0 # 원복
-            
+
+
+# 2회차 풀이 : 231024
+# 각 단어마다 visited 체크
+# backtracking으로 탐색
+def solution(begin, target, words):
+    global answer, visited, l
+    
+    l = len(words)
+    visited = [0] * l
+    answer = l # 단어의 개수 이상만큼 바꿔볼 수 없음
+    
+    if target not in words : return 0
+
+    backtracking(begin, 0, target, words)
+    
+    return answer
+
+def backtracking(cur_word, cnt, target, words) :
+    global answer, visited, l
+    
+    if cur_word == target :
+        answer = min(answer, cnt)
+        return
+    
+    for i in range(l) :
+        if not visited[i] and check(cur_word, words[i]) :
+            visited[i] = 1
+            backtracking(words[i], cnt + 1, target, words)
+            visited[i] = 0
+    
+    
+def check(word1, word2) : # word1 != word2 일 때 비교
+    cnt = 0
+    
+    for i in range(len(word1)) :
+        if word1[i] != word2[i] : cnt += 1
+        if cnt > 1 : return False
+    
+    if cnt == 1 : return True
